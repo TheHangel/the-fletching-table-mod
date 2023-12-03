e58ee0bba2fcf19a030a1d573ba4c3d4402d865d
Hangel <thibaud.lebrasseur@etu.unicaen.fr>
2023-12-03 18:37:57 +0100

    set up

--- src/client/java/dev/hangel/the_fletching_table_mod/TheFletchingTableModClient.java
+++ src/client/java/dev/hangel/the_fletching_table_mod/TheFletchingTableModClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package dev.hangel.the_fletching_table_mod;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class TheFletchingTableModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
--- src/client/java/dev/hangel/the_fletching_table_mod/mixin/client/ExampleClientMixin.java
+++ src/client/java/dev/hangel/the_fletching_table_mod/mixin/client/ExampleClientMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin.client;
+package dev.hangel.the_fletching_table_mod.mixin.client;
 
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
