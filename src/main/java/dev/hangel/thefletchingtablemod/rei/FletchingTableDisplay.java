546d17087acfc3d98bf9e032bc67144a7f1f34ce
Hangel <thibaud.lebrasseur@ecole.ensicaen.fr>
2025-11-11 16:57:05 +0100

    REI integration vary potion and tipped arrow in recipes shown

--- src/main/java/dev/hangel/thefletchingtablemod/rei/FletchingTableDisplay.java
+++ src/main/java/dev/hangel/thefletchingtablemod/rei/FletchingTableDisplay.java
@@ -6,19 +6,34 @@ import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.PotionContentsComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeEntry;
 
 import java.util.List;
 
 public class FletchingTableDisplay extends BasicDisplay {
     public FletchingTableDisplay(RecipeEntry<FletchingTableRecipe> recipe) {
-        super(
-            List.of(
-                EntryIngredients.ofIngredient(recipe.value().getIngredients().get(0)),
-                EntryIngredients.ofIngredient(recipe.value().getIngredients().get(1))
-            ),
-            List.of(EntryIngredient.of(EntryStacks.of(recipe.value().getResult(null))))
-        );
+        super(List.of(), List.of());
+
+        FletchingTableRecipe r = recipe.value();
+
+        EntryIngredient inArrow = EntryIngredients.ofIngredient(r.getIngredients().get(0));
+
+        ItemStack out = r.output().copy();
+        PotionContentsComponent comp = out.get(DataComponentTypes.POTION_CONTENTS);
+
+        ItemStack inPotion = new ItemStack(Items.POTION);
+        if (comp != null) inPotion.set(DataComponentTypes.POTION_CONTENTS, comp);
+
+        EntryIngredient inPotionEntry = EntryIngredient.of(EntryStacks.of(inPotion));
+
+        EntryIngredient outEntry = EntryIngredient.of(EntryStacks.of(out));
+
+        super.inputs  = List.of(inArrow, inPotionEntry);
+        super.outputs = List.of(outEntry);
     }
 
     @Override
