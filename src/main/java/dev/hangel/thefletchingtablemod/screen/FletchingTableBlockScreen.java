a3fb5af44560a9515928c415847aebe4013f19fa
Hangel <thibaud.lebrasseur@ecole.ensicaen.fr>
2025-11-13 22:16:40 +0100

    Screen show potion sprite from minecraft vanilla texture

--- src/main/java/dev/hangel/thefletchingtablemod/screen/FletchingTableBlockScreen.java
+++ src/main/java/dev/hangel/thefletchingtablemod/screen/FletchingTableBlockScreen.java
@@ -2,10 +2,13 @@ package dev.hangel.thefletchingtablemod.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.hangel.thefletchingtablemod.TheFletchingTableMod;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -33,5 +36,17 @@ public class FletchingTableBlockScreen extends HandledScreen<FletchingTableBlock
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
         drawMouseoverTooltip(context, mouseX, mouseY);
+
+        int guiX = (width - backgroundWidth) / 2;
+        int guiY = (height - backgroundHeight) / 2;
+
+        RenderSystem.setShaderColor(0.3f, 0.3f, 0.3f, 1.0f);
+        Sprite sprite = MinecraftClient.getInstance()
+                .getBakedModelManager()
+                .getAtlas(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE)
+                .getSprite(Identifier.ofVanilla("item/potion"));
+
+        context.drawSprite(guiX + 78, guiY + 34, 0, 16, 16, sprite);
+        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
     }
 }
