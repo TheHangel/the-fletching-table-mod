fea6a3a36c4576d19a903f52a651af1eb0aa4b2d
Hangel <thibaud.lebrasseur@ecole.ensicaen.fr>
2025-11-14 21:41:38 +0100

    GUI using vanilla sprite & diaporama potions

--- src/main/java/dev/hangel/thefletchingtablemod/screen/FletchingTableBlockScreen.java
+++ src/main/java/dev/hangel/thefletchingtablemod/screen/FletchingTableBlockScreen.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.text.Text;
@@ -29,24 +30,50 @@ public class FletchingTableBlockScreen extends HandledScreen<FletchingTableBlock
         int x = (width - backgroundWidth) / 2;
         int y = (height - backgroundHeight) / 2;
 
+        MinecraftClient client = MinecraftClient.getInstance();
+        SpriteAtlasTexture atlas  = client.getBakedModelManager().getAtlas(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE);
+
         context.drawTexture(GUI_TEXTURE, x, y, 0, 0, backgroundWidth, backgroundHeight);
+
+        RenderSystem.setShaderColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        Sprite arrowSprite = atlas.getSprite(Identifier.ofVanilla("item/arrow"));
+
+        Sprite potionSprite = atlas.getSprite(Identifier.ofVanilla("item/potion"));
+
+        Sprite splashPotionSprite = atlas.getSprite(Identifier.ofVanilla("item/splash_potion"));
+
+        Sprite lingeringPotionSprite = atlas.getSprite(Identifier.ofVanilla("item/lingering_potion"));
+
+        Sprite tippedArrowSpriteBase = atlas.getSprite(Identifier.ofVanilla("item/tipped_arrow_base"));
+
+        Sprite tippedArrowSpriteHead = atlas.getSprite(Identifier.ofVanilla("item/tipped_arrow_head"));
+
+        context.drawSprite(x + 25, y + 34, 0, 16, 16, arrowSprite);
+
+        long time = client.world != null ? client.world.getTime() : 0;
+
+        int periodTicks = 20;
+        int index = (int) ((time / periodTicks) % 3);
+
+        Sprite current;
+        switch (index) {
+            case 0 -> current = potionSprite;
+            case 1 -> current = splashPotionSprite;
+            default -> current = lingeringPotionSprite;
+        }
+
+        context.drawSprite(x + 78, y + 34, 0, 16, 16, current);
+
+        context.drawSprite(x + 132, y + 34, 0, 16, 16, tippedArrowSpriteBase);
+        context.drawSprite(x + 132, y + 34, 0, 16, 16, tippedArrowSpriteHead);
+
+        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
     }
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
         drawMouseoverTooltip(context, mouseX, mouseY);
-
-        int guiX = (width - backgroundWidth) / 2;
-        int guiY = (height - backgroundHeight) / 2;
-
-        RenderSystem.setShaderColor(0.3f, 0.3f, 0.3f, 1.0f);
-        Sprite sprite = MinecraftClient.getInstance()
-                .getBakedModelManager()
-                .getAtlas(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE)
-                .getSprite(Identifier.ofVanilla("item/potion"));
-
-        context.drawSprite(guiX + 78, guiY + 34, 0, 16, 16, sprite);
-        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
     }
 }
