a0180f5ba0195d28da8e330c268ecbe9c7ed21d6
Hangel <thibaud.lebrasseur@ecole.ensicaen.fr>
2025-11-18 22:37:13 +0100

    fix import

--- src/main/java/dev/hangel/thefletchingtablemod/TheFletchingTableMod.java
+++ src/main/java/dev/hangel/thefletchingtablemod/TheFletchingTableMod.java
@@ -15,7 +15,6 @@ import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
