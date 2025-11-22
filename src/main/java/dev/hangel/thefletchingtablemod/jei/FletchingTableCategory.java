7c80f5538e2cb17eef27bd83b6379906afbd5f9c
Hangel <thibaud.lebrasseur@ecole.ensicaen.fr>
2025-11-22 12:11:34 +0100

    Neoforge port : JEI

---
+++ src/main/java/dev/hangel/thefletchingtablemod/jei/FletchingTableCategory.java
@@ -0,0 +1,63 @@
+package dev.hangel.thefletchingtablemod.jei;
+
+import dev.hangel.thefletchingtablemod.TheFletchingTableMod;
+import dev.hangel.thefletchingtablemod.recipe.FletchingTableRecipe;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FletchingTableCategory implements IRecipeCategory<FletchingTableRecipe> {
+    public static final ResourceLocation UUID = ResourceLocation.fromNamespaceAndPath(TheFletchingTableMod.MOD_ID, "fletching_table");
+    public static final ResourceLocation GUI_TEXTURE =
+            ResourceLocation.fromNamespaceAndPath(TheFletchingTableMod.MOD_ID, "textures/gui/fletching_table_gui.png");
+
+    public static final RecipeType<FletchingTableRecipe> FLETCHING_TABLE_RECIPE_TYPE =
+            new RecipeType<>(UUID, FletchingTableRecipe.class);
+
+    private final IDrawable background;
+    private final IDrawable icon;
+
+    public FletchingTableCategory(IGuiHelper helper) {
+        this.background = helper.createDrawable(GUI_TEXTURE, 0, 0, 176, 80);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(Blocks.FLETCHING_TABLE));
+    }
+
+    @Override
+    public @NotNull RecipeType<FletchingTableRecipe> getRecipeType() {
+        return FLETCHING_TABLE_RECIPE_TYPE;
+    }
+
+    @Override
+    public @NotNull Component getTitle() {
+        return Component.translatable("block.minecraft.fletching_table");
+    }
+
+    @Override
+    public @Nullable IDrawable getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public @Nullable IDrawable getBackground() {
+        return this.background;
+    }
+
+    @Override
+    public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull FletchingTableRecipe recipe, @NotNull IFocusGroup focuses) {
+        builder.addSlot(RecipeIngredientRole.INPUT, 25, 34).addIngredients(recipe.arrowInput());
+        builder.addSlot(RecipeIngredientRole.INPUT, 78, 34).addIngredients(recipe.potionInput());
+
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 132, 34).addItemStack(recipe.getResultItem(null));
+    }
+}
---
+++ src/main/java/dev/hangel/thefletchingtablemod/jei/FletchingTableJEIPlugin.java
@@ -0,0 +1,164 @@
+package dev.hangel.thefletchingtablemod.jei;
+
+import dev.hangel.thefletchingtablemod.TheFletchingTableMod;
+import dev.hangel.thefletchingtablemod.recipe.FletchingTableRecipe;
+import dev.hangel.thefletchingtablemod.screen.FletchingTableBlockScreen;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Objects;
+
+@JeiPlugin
+public class FletchingTableJEIPlugin implements IModPlugin {
+    @Override
+    public @NotNull ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(TheFletchingTableMod.MOD_ID, "jei_fletching_table");
+    }
+
+    @Override
+    public void registerCategories(@NotNull IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new FletchingTableCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(@NotNull IRecipeRegistration registration) {
+        RecipeManager recipeManager = Objects.requireNonNull(Minecraft.getInstance().getConnection()).getRecipeManager();
+
+        List<FletchingTableRecipe> jeiRecipes = new java.util.ArrayList<>();
+
+        List<RecipeHolder<FletchingTableRecipe>> holders =
+                recipeManager.getAllRecipesFor(TheFletchingTableMod.FLETCHING_TABLE_RECIPE_TYPE.get());
+
+        for (RecipeHolder<FletchingTableRecipe> holder : holders) {
+            FletchingTableRecipe base = holder.value();
+
+            Ingredient arrowIng  = base.arrowInput();
+            Ingredient potionIng = base.potionInput();
+
+            ItemStack[] potionBases = potionIng.getItems();
+
+            boolean allowNormal    = java.util.Arrays.stream(potionBases).anyMatch(s -> s.is(Items.POTION));
+            boolean allowSplash    = java.util.Arrays.stream(potionBases).anyMatch(s -> s.is(Items.SPLASH_POTION));
+            boolean allowLingering = java.util.Arrays.stream(potionBases).anyMatch(s -> s.is(Items.LINGERING_POTION));
+
+            if (!allowNormal && !allowSplash && !allowLingering) {
+                jeiRecipes.add(base);
+                continue;
+            }
+
+            for (Potion potion : BuiltInRegistries.POTION) {
+                ResourceLocation id = BuiltInRegistries.POTION.getKey(potion);
+                if (id != null) {
+                    String path = id.getPath();
+                    if (path.equals("water") || path.equals("awkward") ||
+                            path.equals("thick") || path.equals("mundane")) {
+                        continue;
+                    }
+                }
+
+                ResourceLocation potionId = BuiltInRegistries.POTION.getKey(potion);
+                if (potionId == null) continue;
+
+                ResourceKey<Potion> key = ResourceKey.create(Registries.POTION, potionId);
+
+                Holder.Reference<Potion> potionHolder = BuiltInRegistries.POTION.getHolder(key).orElse(null);
+
+                if (potionHolder == null) continue;
+
+                PotionContents contents = new PotionContents(
+                        java.util.Optional.of(potionHolder),
+                        java.util.Optional.empty(),
+                        java.util.List.of()
+                );
+
+                if (allowNormal) {
+                    ItemStack normal = new ItemStack(Items.POTION);
+                    normal.set(DataComponents.POTION_CONTENTS, contents);
+
+                    if (!normal.getHoverName().getString().equals(Component.translatable("item.minecraft.potion.effect.empty").getString())) {
+
+                        ItemStack out = new ItemStack(Items.TIPPED_ARROW);
+                        out.set(DataComponents.POTION_CONTENTS, contents);
+
+                        FletchingTableRecipe jeiRecipe = new FletchingTableRecipe(
+                                arrowIng,
+                                Ingredient.of(normal),
+                                out
+                        );
+                        jeiRecipes.add(jeiRecipe);
+                    }
+                }
+
+                if (allowSplash) {
+                    ItemStack splash = new ItemStack(Items.SPLASH_POTION);
+                    splash.set(DataComponents.POTION_CONTENTS, contents);
+
+                    if (!splash.getHoverName().getString().equals(Component.translatable("item.minecraft.splash_potion.effect.empty").getString())) {
+
+                        ItemStack out = new ItemStack(Items.TIPPED_ARROW);
+                        out.set(DataComponents.POTION_CONTENTS, contents);
+
+                        FletchingTableRecipe jeiRecipe = new FletchingTableRecipe(
+                                arrowIng,
+                                Ingredient.of(splash),
+                                out
+                        );
+                        jeiRecipes.add(jeiRecipe);
+                    }
+                }
+
+                if (allowLingering) {
+                    ItemStack lingering = new ItemStack(Items.LINGERING_POTION);
+                    lingering.set(DataComponents.POTION_CONTENTS, contents);
+
+                    if (!lingering.getHoverName().getString().equals(Component.translatable("item.minecraft.lingering_potion.effect.empty").getString())) {
+
+                        ItemStack out = new ItemStack(Items.TIPPED_ARROW);
+                        out.set(DataComponents.POTION_CONTENTS, contents);
+
+                        FletchingTableRecipe jeiRecipe = new FletchingTableRecipe(
+                                arrowIng,
+                                Ingredient.of(lingering),
+                                out
+                        );
+                        jeiRecipes.add(jeiRecipe);
+                    }
+                }
+            }
+        }
+
+        registration.addRecipes(FletchingTableCategory.FLETCHING_TABLE_RECIPE_TYPE, jeiRecipes);
+    }
+
+    @Override
+    public void registerGuiHandlers(@NotNull IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(FletchingTableBlockScreen.class, 100, 34, 25, 20, FletchingTableCategory.FLETCHING_TABLE_RECIPE_TYPE);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(@NotNull IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(Blocks.FLETCHING_TABLE.asItem()), FletchingTableCategory.FLETCHING_TABLE_RECIPE_TYPE);
+    }
+}
